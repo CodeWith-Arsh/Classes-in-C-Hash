76bf5304fc22af42ec1029eaef9006d8d0e56b93
arshid ahmad <arshid.ahmad1@gmail.com>
2024-07-09 14:09:14 +0530

    Add project files.

---
+++ ConsoleApp2/Bike.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyApplication
+{
+    class Car : Vehicle  // Derived class
+    {
+        public string modelName = "Mustang";  // Car field
+    }
+}
---
+++ ConsoleApp2/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using MyApplication;
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApplication
+{
+     class Vehicle
+    {
+        public string brand;
+        public void honk()
+            {
+            Console.WriteLine("Hello");
+        }
+    }
+}
