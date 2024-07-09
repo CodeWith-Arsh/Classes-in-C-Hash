813cb7854a3b19c0312b7be8ef3b37cd25a9c274
arshid ahmad <arshid.ahmad1@gmail.com>
2024-07-09 14:54:43 +0530

    Polymorphism

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,7 +2,8 @@
 
 namespace MyApplication
 {
-    class Program
+    //Classes Derived and Base Classes
+    /*class Program
     {
         static void Main(string[] args)
         {
@@ -15,5 +16,45 @@ namespace MyApplication
             // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
             Console.WriteLine(myCar.brand + " " + myCar.modelName);
         }
-    }
+    }*/
+
+    //Polymorphism and Overriding Methods
+/*class Animal
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("This Animal Makes a Sound");
+        }
+        class Pigeon : Animal
+        {
+            public void animalSound()
+            { 
+                Console.WriteLine("this is Pigeon sound:Koo KOO");
+            }
+            class Dog : Animal 
+            {
+                public void animalSound()
+                {
+                    Console.WriteLine("This is Dog Sound Boo BOo");
+                }
+                class Program
+                {
+                    static void Main(string[] args)
+                    {
+                        Animal myAnimal = new Animal();
+                        Animal myPigeon = new Animal();
+                        Animal myDog= new Animal();
+                        myAnimal.animalSound();
+                        myPigeon.animalSound();
+                        myDog.animalSound();
+                        Console.WriteLine("These are the examples of the Polymorphism");
+
+                    }
+                }
+            }
+        }
+    }*/
+
+
+
 }
